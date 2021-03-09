f89a10cbe82b47a00f7e598bf6cb2b21edc102df
Jeff Umandap <jeffumandap@Jeffs-MacBook-Pro.local>
2021-03-09 12:00:48 +0800

    Custom drawing with drawRext (Currency symbol for textfields) completed

--- window-shopper/View/CurrencyTxtFld.swift
+++ window-shopper/View/CurrencyTxtFld.swift
@@ -10,28 +10,30 @@ import UIKit
 @IBDesignable
 class CurrencyTxtFld: UITextField {
     
+    override func draw(_ rect: CGRect) {
+        let size : CGFloat = 20
+        let currencyLbl = UILabel(frame: CGRect(x: 5, y: (frame.size.height / 2) - size / 2, width: size, height: size))
+        currencyLbl.backgroundColor = #colorLiteral(red: 0.793530345, green: 0.7936646342, blue: 0.793512702, alpha: 0.798553037)
+        currencyLbl.textAlignment = .center
+        currencyLbl.textColor = #colorLiteral(red: 0.2549019754, green: 0.2745098174, blue: 0.3019607961, alpha: 1)
+        currencyLbl.layer.cornerRadius = 5.0
+        currencyLbl.clipsToBounds = true
+        
+        let formatter = NumberFormatter()
+        formatter.numberStyle = .currency
+        formatter.locale = .current
+        currencyLbl.text = formatter.currencySymbol
+        
+        addSubview(currencyLbl)
+    }
+    
     override func prepareForInterfaceBuilder() {
         customizeView()
     }
     
-    
     override func awakeFromNib() {
         super.awakeFromNib()
         customizeView()
-        
-        
-        
-//        let tf = CurrencyTxtFld()
-//
-//        tf.layer.backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 0.25)
-//        tf.layer.cornerRadius = 5.0
-//        tf.textAlignment = .center
-//
-//        if let p = tf.self.placeholder {
-//            let plc = NSAttributedString(string: p, attributes: [.foregroundColor: #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)])
-//            tf.attributedPlaceholder = plc
-//            tf.textColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
-//        }
         
     }
     
@@ -39,6 +41,8 @@ class CurrencyTxtFld: UITextField {
         backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 0.25)
         layer.cornerRadius = 5.0
         textAlignment = .center
+        
+        clipsToBounds = true
         
         if let p = placeholder {
             let place = NSAttributedString(string: p, attributes: [.foregroundColor: #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)])
